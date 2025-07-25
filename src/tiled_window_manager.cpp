50bab1baa8f6c00fc3d29bf0dcf8d4983f542e1e
Borja Castellano <borjacastellano1@gmail.com>
2025-07-25 14:28:13 +0200

    the update windows call wasn't useless

--- src/tiled_window_manager.cpp
+++ src/tiled_window_manager.cpp
@@ -212,6 +212,8 @@ bool TiledWindowManager::handle_keyboard_event(MirKeyboardEvent const* event)
             {
                 --this->current_workspace_index;
             }
+            
+            update_windows({});
 
             return true;
         case XKB_KEY_m:
